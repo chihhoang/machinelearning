cfeb5635d10d867210f0881be6300aa19327d307
Chi Hoang <chhoang102@gmail.com>
2018-10-09 00:08:00 -0700

    temp commit

--- logistic-regression/costFunctionReg.m
+++ logistic-regression/costFunctionReg.m
@@ -16,7 +16,21 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+h = sigmoid(X * theta);
+% J = costFunction(theta, X, y)(1) + lambda/;
 
+% TODO: Analyze dimension and finish gradient
+J = 1 / m * (-y'*log(h) - (1 - y')*log(1 - h)) + lambda/2/m*theta.^2;
+disp(size(h));
+disp(size(J));
+first_x = X(:,1);
+disp(size(X));
+disp(size(first_x'));
+
+h0 = sigmoid(X * theta);
+grad(1) = grad(1) + 1/m*first_x'*(h(1,:) - y(1,:));
+
+grad() = grad + 1/m*X(2:m,:)'*(h(2:m,:) - y(2:m)) + lambda/m*theta;
 
 
 
