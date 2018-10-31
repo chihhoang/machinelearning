15d7601d641ad1374930f02245b27e4262256a84
Chi Hoang <chhoang102@gmail.com>
2018-10-31 00:35:01 -0700

    Cross validate the SVM to determine the best C and sigma parameters

--- support-vector-machines/dataset3Params.m
+++ support-vector-machines/dataset3Params.m
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01 0.03 0.1 0.3 1 3 10 30];
+min_error = 999999;
 
+% Use the result (from the below code) directly 
+% to avoid long training during submission
+% C = 1;
+% sigma = 0.1;
 
-
-
-
+for i = 1:length(steps)
+    C_ = steps(i);
+    for j = 1:length(steps)
+        sigma_ = steps(j);
+        % Train the SVM with gaussian kernel
+        model = svmTrain(X, y, C_, @(x1, x2) gaussianKernel(x1, x2, sigma_));
+        % Predict the labels on the cross validation set
+        predictions = svmPredict(model, Xval);
+        current_error = mean(double(predictions ~= yval));
+        % If the current error is less than the minimum error so far
+        % update C and sigma
+        if current_error < min_error
+            min_error = current_error;
+            C = C_;
+            sigma = sigma_;
+        endif
+    end
+end
 
 % =========================================================================
 
