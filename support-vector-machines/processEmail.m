5cae4f10677a4b4d6ccf4b2ce9d0736e9616dacb
Chi Hoang <chhoang102@gmail.com>
2018-10-31 21:31:35 -0700

    Map each word in the preprocessed emails into a list of word indices that contains the index of the word in the vocab list

--- support-vector-machines/processEmail.m
+++ support-vector-machines/processEmail.m
@@ -96,15 +96,14 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
+    
+    % match contains a vector of 0 and 1 in which
+    % 1 if the character strings str and vocabList are the same, and 0 otherwise
+    match = strcmp(str, vocabList);
 
-
-
-
-
-
-
-
-
+    % find returns a list of indexes of non zero elements in match
+    % then we append the list to word_indices column wise
+    word_indices = [word_indices; find(match)];
 
     % =============================================================
 
